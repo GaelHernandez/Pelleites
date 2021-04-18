50969e0e29a5cf7866df433905905f8c6fff1561
Gabriel <Gabriel@gmail.com>
2021-04-17 20:53:49 -0400

    Menu creado

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menu_a = document.getElementById('header__menuh--icon');
+const menu_a_2 = document.getElementById('menu__bg');
+
+function cambio() {
+    menu_a.classList.toggle('menu_a');
+    menu_a_2.classList.toggle('menu_a_2');
+};
